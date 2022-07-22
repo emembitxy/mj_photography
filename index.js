bdff746ac7147f6bf24639ae1bc17e8d1b2cbb53
emembitxy <ememessien212@yahoo.com>
2022-07-22 11:40:08 +0100

    start

---
+++ index.js
@@ -0,0 +1,8 @@
+const article = document.querySelector(input)
+const option = document.querySelector(option)
+let option=[...]
+option.push()
+for(let index=0;index<option,option++){
+    console.log(option)
+}
+article.addEventListener("submit", article);
