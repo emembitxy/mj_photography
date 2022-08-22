c060b690526bfd06933650d4f4d9459d3a60ee33
emembitxy <ememessien212@yahoo.com>
2022-08-22 11:00:55 +0100

    start

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const form = document.querySelector(input)
-const
-form.addEventListener("submit", article);
+const figureimage =document.querySelector(".aboutusfigure1")
+const background=["images/mj_1.jpg"]
+function updateimage(){
+    fetch(background)
+    .then((Response)=>Response.json())
+    .then((data)=>{
+        const figureimage=data.value
+    })
+    console.log(updateimage)
+}
+
